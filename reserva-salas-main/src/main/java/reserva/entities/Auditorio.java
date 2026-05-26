ca391db960607362dbf0ed744fc96a392fb431cd
c.pineda <c.pineda@unillanos.edu.co>
2026-05-26 09:52:45 -0500

    Add files via upload

---
+++ reserva-salas-main/src/main/java/reserva/entities/Auditorio.java
@@ -0,0 +1,8 @@
+package reserva.entities;
+
+public class Auditorio  extends Sala {
+
+    public Auditorio(String id, String nombre, int capacidad, String ubicacion) {
+        super(id, nombre, capacidad, ubicacion);
+    }
+}
